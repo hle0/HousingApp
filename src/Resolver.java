825e67c9610c76c9a3bcb849b7aac58e35c14360
Mario Egocheaga <43833659+Mario-Egocheaga@users.noreply.github.com>
2022-04-02 19:49:04 -0400

    Adding functions

--- src/Resolver.java
+++ src/Resolver.java
@@ -1,3 +1,5 @@
+import java.io.Serializable;
+
 // Implementations of Resolver implement a strategy for allocating rooms to students fairly.
 public interface Resolver {
     // Allocate rooms given student requests and available room data.
@@ -10,9 +12,60 @@ public interface Resolver {
 
 class Solve implements Resolver
 {
+    int id;
+    int spaceAvailable;
+    String name;
+    Housing housing;
+
+    List <User> students;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public List<Student> getListOfStudents()
+    {
+        return listOfStudents();
+
+    }
+
+    public void setListOfStudents(List<Student> listOfStudents)
+    {
+        this.listOfStudents = listOfStudents;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Housing getHousing()
+    {
+        return housing;
+    }
+
+    public List<User> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<User> students) {
+        this.students = students;
+    }
+
+
 
     public void roomAllocation()
     {
-        
+
     }
+}
+
+public class Housing extends User implements Serializable
+{
+    String name;
+    String housing;
+    String group;
+
+
 }
