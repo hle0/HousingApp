fa629c9cde4013e8364ff2e226fd71ffb45f0d4d
Mario Egocheaga <43833659+Mario-Egocheaga@users.noreply.github.com>
2022-04-02 16:30:56 -0400

    Started implementing resolver

--- src/Resolver.java
+++ src/Resolver.java
@@ -1,6 +1,18 @@
 // Implementations of Resolver implement a strategy for allocating rooms to students fairly.
 public interface Resolver {
     // Allocate rooms given student requests and available room data.
+    public void roomAllocation();
+    public void studentRequests();
+    public void availableRooms();
     // See sample .csv files.
     CSVFile resolve(CSVFile housingData, CSVFile studentChoices);
+}
+
+class Solve implements Resolver
+{
+
+    public void roomAllocation()
+    {
+        
+    }
 }
